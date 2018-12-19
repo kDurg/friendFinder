333157990c47f5173f29579491d295340436d270
kDurg <kyle.durigan@gmail.com>
2018-12-19 16:12:09 -0700

    file and folders in correct format. Bootstrapped a quick homepage (index.html). added boilerplate code for mysql, express and handlebars

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require("express");
+var app = express();
+var mysql = require("mysql");
+var exphbs = require("express-handlebars");
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "root",
+  database: ""
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+
+  console.log("connected as id " + connection.threadId);
+});
